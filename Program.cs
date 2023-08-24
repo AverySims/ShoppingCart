8198a964cd243840f14866e7a06fdcee5eb07942
AverySims <avery.j.sims@gmail.com>
2023-08-24 14:33:11 -0500

    Added external C# file to project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace ShoppingCart
 {
+	public class Item
+	{
+		public Item(string name, double price)
+		{
+			Name = name;
+			Price = price;
+		}
+		public string Name { get; }
+		public double Price { get; }
+	}
+    
 	internal class Program
 	{
+		private static readonly string[] menuOptions =
+			{ "Add item to cart", "View items in cart", "See total price of cart", "Exit program" };
+		
+		private static List<Item> cart = new List<Item>();
+		
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			bool loopMain = true;
+			
+			while (loopMain)
+			{
+				Console.WriteLine("- - - Menu - - -");
+				for (int i = 0; i < menuOptions.Length; i++)
+				{
+					Console.WriteLine($"{i + 1}. {menuOptions[i]}");
+				}
+
+				Console.ReadLine();
+			}
 		}
+		
+		
+		
 	}
 }
